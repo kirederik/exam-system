a1dab79f8214a766b57b636737c6ab52a1f437bc
Derik Evangelista <kirederik@gmail.com>
2013-09-16 00:13:27 -0300

    Models Schema
    Signed-off-by: Derik Evangelista <kirederik@gmail.com>

--- app/Config/Schema/myModels.sql
+++ app/Config/Schema/myModels.sql
@@ -21,8 +21,11 @@ create table disciplines_categories (
     id int unsigned not null auto_increment primary key,
     discipline_id int not null,
     category_id int not null,
+    created DATETIME DEFAULT NULL,
+    modified DATETIME DEFAULT NULL,
     discipline_id foreign key references disciplines(id),
     category_id foreign key references categories(id)
+
 );
 
 create table questions (
@@ -32,7 +35,49 @@ create table questions (
     wrong_comment text,
     show_correct boolean default true,
     show_wrong boolean default true,
+    created DATETIME DEFAULT NULL,
+    modified DATETIME DEFAULT NULL,
     discipline_id int not null,
     discipline_id foreign key references disciplines(id),
 );
+
+create table alternatives (
+    id int unsigned not null auto_increment primary key,
+    alt_text text not null,
+    created DATETIME DEFAULT NULL,
+    modified DATETIME DEFAULT NULL,
+);
+
+create table questions_alternatives(
+    id int unsigned not null auto_increment primary key,
+    question_id int not null,
+    alternative_id int not null,
+    created DATETIME DEFAULT NULL,
+    modified DATETIME DEFAULT NULL,
+    question_id foreign key references questions(id)
+    alternative_id foreign key references alternatives(id)
+);
+
+create table exercises(
+    id int unsigned not null auto_increment primary key,
+    discipline_id int not null,
+    quantity int not null,
+    created DATETIME DEFAULT NULL,
+    modified DATETIME DEFAULT NULL,
+    discipline_id foreign key references disciplines(id)
+);
+
+create table exams(
+    id int unsigned not null auto_increment primary key,
+    category_id int not null
+);
+
+create table exams_disciplines(
+    id int unsigned not null auto_increment primary key,
+    discipline_id int not null,
+    exam_id int not null,
+    discipline_id foreign key references disciplines(id),
+    exam_id foreign key references exams(id)
+);
+
 insert into disciplines (name, created) values ('Combate a Incêndio', NOW());
